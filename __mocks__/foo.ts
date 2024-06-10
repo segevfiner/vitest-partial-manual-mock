509d4da9fef5768c8e1d55ed24cea3dfde912fee
Segev Finer <segev@swimm.io>
2024-06-10 14:54:22 +0300

    Add comment

--- __mocks__/foo.ts
+++ __mocks__/foo.ts
@@ -1,5 +1,6 @@
 import { vi } from 'vitest';
 
+// Doesn't exist
 const mod = vi.createMockFromModule<typeof import('../foo')>('../foo');
 // Doesn't work, infinite loop
 // const mod = vi.importMock<typeof import('../foo')>('../foo');
